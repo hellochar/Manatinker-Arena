3c6bc9c2791095b0f509fbde0df47221d73a3c82
Xiaohan Zhang <hellocharlien@hotmail.com>
2022-03-19 15:30:10 -0700

    give shields some personality

--- Assets/src/model/fragments/Shields.cs
+++ Assets/src/model/fragments/Shields.cs
@@ -7,16 +7,18 @@ public abstract class Shield : Fragment {
   public override float myInFlowRate => 5;
   public override float myManaMax => myHpMax / 2;
   public override float myOutFlowRate => 0;
+  public virtual float absorptionPercent => 0.5f;
+  public virtual float manaToDamageRatio => 3;
 
-  public override string Description => "Redirects 50% of damage to Mana (3 Mana : 1 damage).";
+  public override string Description => $"Redirects {Mathf.RoundToInt(absorptionPercent * 100)}% of damage to Mana ({manaToDamageRatio} Mana : 1 damage).";
 
   public override void ChangeHP(float diff) {
     // take mana damage first
     if (diff < 0) {
-      var dmgToSoak = -diff / 2;
-      var manaCost = dmgToSoak * 3;
+      var dmgToSoak = -diff * absorptionPercent;
+      var manaCost = dmgToSoak * manaToDamageRatio;
       var manaUsed = Mathf.Min(Mana, manaCost);
-      var dmgTaken = manaUsed / 3;
+      var dmgTaken = manaUsed / manaToDamageRatio;
       diff += dmgTaken;
       ChangeMana(-manaUsed);
       base.ChangeHP(diff);
@@ -28,14 +30,17 @@ public abstract class Shield : Fragment {
 
 [RegisteredFragment]
 public class Buckler : Shield {
-  public override float myHpMax => 60;
+  public override float myHpMax => 30;
+  public override float myManaMax => 30;
   public override float weight => 2f;
+  public override float absorptionPercent => 0.8f;
   public Buckler() {
   }
 }
 
 [RegisteredFragment]
 public class TowerShield : Shield {
+  public override float myInFlowRate => 3;
   public override float myHpMax => 120;
   public override float weight => 10f;
   public TowerShield() {
@@ -52,8 +57,10 @@ public class Crenel : Shield {
 
 [RegisteredFragment]
 public class DotMatrix : Shield {
-  public override float myHpMax => 60;
+  public override float myHpMax => 10;
+  public override float myManaMax => 60;
   public override float weight => 1f;
+  public override float absorptionPercent => 1;
   public DotMatrix() {
   }
 }
